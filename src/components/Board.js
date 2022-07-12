cc87539b8f83675c33ad2a4be8756e761d3b4c65
Aaron Collins <aa.collins.95@gmail.com>
2022-07-12 13:00:57 -0400

    distriutes mines

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,12 @@ import Cell from './Cell'
 //import '../styles/App.css';
 
 const Board = (props) => {
-    const cols = 18;
-    const rows = 14;
-    const mines = 40;
-    
+    const cols = 10;
+    const rows = 10;
+    const mines = 5;
+    /*
     const seedMines = (r, c, m) => {
+        //seeds mines in a 2d array
         const placedMines = []
         while (placedMines.length < m) {
             let randRow = Math.floor(Math.random() * r)
@@ -19,28 +20,50 @@ const Board = (props) => {
                 placedMines.push(coord)
             }
         }
-        
         console.log(placedMines)
-        //console.log(`Length = ${placedMines.length}`)
-        console.log('hi')
+        return placedMines
+    }
+    */
+    const seedMines = (r, c, m) => {
+        //seeds mines as a single number
+        const placedMines = []
+        while (placedMines.length < m) {
+            let location = Math.floor(Math.random() * r * c)
+            if (! placedMines.includes(location)) {
+                placedMines.push(location)
+            }
+        }
+        console.log(placedMines)
         return placedMines
     }
 
-    const generateRow = (row, cols) => {
+    const convertRC = (row, col) => {
+        return row * 10 + col
+    }
+
+    const generateRow = (row, cols, placedMines) => {
         var columns = [];
+        let isMine = false;
         for (var col = 0; col < cols; col++) {
-            // note: we are adding a key prop here to allow react to uniquely identify each
-            // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-            columns.push(<Cell key={`${col}-${row}`} />);
+            //console.log(placedMines.includes(convertRC(row, col)))
+            if (placedMines.includes(convertRC(row, col))) {
+                //checks if there is a mine here based on the random seed
+                
+                isMine = true
+            }
+            columns.push(<Cell isMine={isMine} key={`${col}-${row}`} />);
+            isMine = false
         }
         //return <div className='cell-row'>{columns}</div>;
         return <div className='row'>{columns}</div>;
     }
 
+
+
     const generateBoard = (rows, cols, placedMines) => {
         const cellBoard = []
         for (var row = 0; row < rows; row++) {
-            cellBoard.push(<div className='row' key={row}>{generateRow(row, cols)}</div>)
+            cellBoard.push(<div className='row' key={row}>{generateRow(row, cols, placedMines)}</div>)
         }
         return <div className='cell-table'>{cellBoard}</div>
     }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,11 +5,18 @@ import '../styles/Cell.css';
 const Cell = (props) => {
     //const [fieldText, setFieldText] = useState('')
     //const fieldName = props.fieldName
+    let isMine = props.isMine
+    let status;
 
+    if (isMine) {
+        status = 'M'
+    } else {
+        status = ''
+    }
 
     return (
         <div className="cell">
-            
+            <p className='status'>{status}</p>
         </div>
     )
 }
